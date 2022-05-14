0d1a64187284e55fa5753bed0b986f6086fb1a81
CrimsonBart <99059557+CrimsonBart@users.noreply.github.com>
2022-05-14 13:09:01 -0700

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,10 @@
+import java.util.*;
+/**
+ * import statements
+ * methods that will print the Name of the User
+ * -print the total points
+ * -print the total streak days
+ * -print the start date
+ */
+public class Profile  {
+}
