aa2056a09543cf79708d950d47baf0635a9261f8
ThePokepro95 <77712820+ThePokepro95@users.noreply.github.com>
2022-05-13 23:13:02 -0700

    driver progress

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Driver {
+import java.util.Scanner;
+public class Driver {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Welcome to Calc 1!");
+        System.out.println("Please enter your name! If you already have a profile, please type in '0'.");
+        String name = input.next();
+        if (name.equals("0")){
+            System.out.println("Welcome back! Enter your name.");
+
+        }
+
+    }
 }
